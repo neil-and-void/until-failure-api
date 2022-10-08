1cb1b826f147bb9eb5d9f4d3ec2ea03dc05b3cd2
Neilzon <neilcviloria@gmail.com>
2022-10-08 15:02:34 -0600

    disable prepared statements

--- common/database/database.go
+++ common/database/database.go
@@ -1,20 +1,25 @@
 package database
 
 import (
+	"fmt"
 	"os"
 
 	"gorm.io/driver/postgres"
 	"gorm.io/gorm"
-	"gorm.io/gorm/logger"
 )
 
 func InitDb() (*gorm.DB, error) {
-	databaseUrl := os.Getenv("DATABASE_URL")
-
+	DB_HOST := os.Getenv("DB_HOST")
+	DB_DBNAME := os.Getenv("DB_DBNAME")
+	DB_USERNAME := os.Getenv("DB_USERNAME")
+	DB_PASSWORD := os.Getenv("DB_PASSWORD")
+	DB_PORT := os.Getenv("DB_PORT")
+	DSN := fmt.Sprintf("host=%s user=%s password=%s dbname=%s port=%s sslmode=disable", DB_HOST, DB_USERNAME, DB_PASSWORD, DB_DBNAME, DB_PORT)
 	var err error
-	db, err := gorm.Open(postgres.Open(databaseUrl), &gorm.Config{
-		Logger: logger.Default.LogMode(logger.Silent),
-	})
+	db, err := gorm.Open(postgres.New(postgres.Config{
+		DSN: DSN,
+		PreferSimpleProtocol: true, // disables implicit prepared statement usage
+	  }), &gorm.Config{})
 	if err != nil {
 		return nil, err
 	}
