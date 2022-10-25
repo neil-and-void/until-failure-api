bea650a348144098a5b14cfc8c618766c6613386
Neilzon <neilcviloria@gmail.com>
2022-10-24 21:35:58 -0600

    workout seession access controller

--- accesscontrol/accesscontrol.go
+++ accesscontrol/accesscontrol.go
@@ -23,6 +23,18 @@ func (ac *AccessController) CanAccessWorkoutRoutine(userId string, workoutRoutin
 	return nil
 }
 
+func (ac *AccessController) CanAccessWorkoutSession(userId string, workoutSessionId string) error {
+	_, err := database.GetWorkoutSession(ac.DB, userId, workoutSessionId)
+	if errors.Is(err, gorm.ErrRecordNotFound) {
+		return errors.New("Access Denied")
+	}
+	if err != nil {
+		return err
+	}
+
+	return nil
+}
+
 func NewAccessController(db *gorm.DB) *AccessController {
 	return &AccessController{
 		DB: db,
--- accesscontrol/accesscontrol_test.go
+++ accesscontrol/accesscontrol_test.go
@@ -13,8 +13,9 @@ import (
 
 func TestAccessControl(t *testing.T) {
 	wr := test.WorkoutRoutine
+	ws := test.WorkoutSession
 
-	t.Run("Test Access Control Success", func(t *testing.T) {
+	t.Run("Test Can Access Workout Routine Success", func(t *testing.T) {
 		mock, gormDB := test.SetupMockDB()
 
 		userId := fmt.Sprintf("%d", wr.UserID)
@@ -30,9 +31,14 @@ func TestAccessControl(t *testing.T) {
 		ac := &AccessController{DB: gormDB}
 		err := ac.CanAccessWorkoutRoutine(userId, workoutRoutineId)
 		require.Nil(t, err, "Should be no error for accessing workout routine")
+
+		err = mock.ExpectationsWereMet()
+		if err != nil {
+			panic(err)
+		}
 	})
 
-	t.Run("Test Access Control Denied", func(t *testing.T) {
+	t.Run("Test Can Access Workout Routine Denied", func(t *testing.T) {
 		mock, gormDB := test.SetupMockDB()
 
 		userId := fmt.Sprintf("%d", wr.UserID)
@@ -43,5 +49,52 @@ func TestAccessControl(t *testing.T) {
 		ac := &AccessController{DB: gormDB}
 		err := ac.CanAccessWorkoutRoutine(userId, workoutRoutineId)
 		require.Equal(t, err.Error(), "Access Denied")
+
+		err = mock.ExpectationsWereMet() 
+		if err != nil {
+			panic(err)
+		}
+	})
+
+	t.Run("Test Can Access Workout Session Success", func(t *testing.T) {
+		mock, gormDB := test.SetupMockDB()
+
+		userId := fmt.Sprintf("%d", ws.UserID)
+		workoutSessionId := fmt.Sprintf("%d", ws.ID)
+
+		workoutSessionRow := sqlmock.
+			NewRows([]string{"id", "user_id", "start", "end", "workout_routine_id", "created_at", "deleted_at", "updated_at"}).
+			AddRow(ws.ID, ws.UserID, ws.Start, ws.End, ws.WorkoutRoutineID, ws.CreatedAt, ws.DeletedAt, ws.UpdatedAt)
+
+		const workoutSessionQuery = `SELECT * FROM "workout_sessions" WHERE (user_id = $1 AND id = $2) AND "workout_sessions"."deleted_at" IS NULL ORDER BY "workout_sessions"."id" LIMIT 1`
+		mock.ExpectQuery(regexp.QuoteMeta(workoutSessionQuery)).WithArgs(userId, workoutSessionId).WillReturnRows(workoutSessionRow)
+
+		ac := &AccessController{DB: gormDB}
+		err := ac.CanAccessWorkoutSession(userId, workoutSessionId)
+		require.Nil(t, err, "Should be no error for accessing workout session")
+
+		err = mock.ExpectationsWereMet()
+		if err != nil {
+			panic(err)
+		}
+	})
+
+	t.Run("Test Can Access Workout Session Denied", func(t *testing.T) {
+		mock, gormDB := test.SetupMockDB()
+
+		userId := fmt.Sprintf("%d", ws.UserID)
+		workoutSessionId := fmt.Sprintf("%d", ws.ID)
+
+		const workoutSessionQuery = `SELECT * FROM "workout_sessions" WHERE (user_id = $1 AND id = $2) AND "workout_sessions"."deleted_at" IS NULL ORDER BY "workout_sessions"."id" LIMIT 1`
+		mock.ExpectQuery(regexp.QuoteMeta(workoutSessionQuery)).WithArgs(userId, workoutSessionId).WillReturnError(gorm.ErrRecordNotFound)
+
+		ac := &AccessController{DB: gormDB}
+		err := ac.CanAccessWorkoutSession(userId, workoutSessionId)
+		require.Equal(t, err.Error(), "Access Denied")
+		
+		err = mock.ExpectationsWereMet()
+		if err != nil {
+			panic(err)
+		}
 	})
 }
