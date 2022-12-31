10384c61cd0ece95e3f90480695fa6fe8cc35974
Neilzon <neilcviloria@gmail.com>
2022-12-31 10:50:48 -0700

    comments

--- config/config.go
+++ config/config.go
@@ -5,6 +5,9 @@ import "time"
 const (
 	ACCESS_TTL     time.Duration = 720 // hours
 	REFRESH_TTL    time.Duration = 24  // hours
+
+	// these are not the actual secrets, but are the keys to get the secrets
+	// from the .env file
 	ACCESS_SECRET                = "ACCESS_SECRET"
 	REFRESH_SECRET               = "REFRESH_SECRET"
 )
