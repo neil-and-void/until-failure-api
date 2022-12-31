442bff2a12435f5d88498b1dd87e21294597aa9c
Neilzon <neilcviloria@gmail.com>
2022-12-31 15:50:01 -0700

    improve errors

---
+++ errors/errors.go
@@ -0,0 +1,10 @@
+package errors
+
+// package contains error strings that are expected to be presented to the user
+const (
+	CreateWorkoutRoutineError = "Could not create workout routine, %s"
+	GetWorkoutRoutinesError = "Could not get workout routines, %s"
+	GetWorkoutRoutineError = "Could not get workout routine, %s"
+	UpdateWorkoutRoutineError = "Could not update workout routine, %s"
+	DeleteWorkoutRoutineError = "Could not delete workout routine, %s"
+)
