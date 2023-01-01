5ce13e6b614bbc083dac86e568d4c2f2af42c0b9
Neilzon <neilcviloria@gmail.com>
2022-12-31 20:44:00 -0700

    remove functions from utils

--- utils/utils.go
+++ utils/utils.go
@@ -2,25 +2,8 @@ package utils
 
 import (
 	"fmt"
-	"regexp"
 	"strconv"
 )
-
-// check if password is strong
-func IsStrong(p string) bool {
-	re, err := regexp.Compile("\\d")
-	if err != nil {
-		panic(err)
-	}
-	return re.MatchString(p) && checkLength(p)
-}
-
-func checkLength(p string) bool {
-	if 8 <= len(p) && len(p) <= 18 {
-		return true
-	}
-	return false
-}
 
 func StringToUInt(s string) uint {
 	num, err := strconv.ParseUint(s, 10, strconv.IntSize)
